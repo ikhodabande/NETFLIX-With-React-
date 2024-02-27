49261e2839978266b04f325802cee010400154fa
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-27 10:37:24 +0330

    import routes and route from react-router-dom

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import {Routes,Route}  from 'react-router-dom'
 
 function App() {
   return (
