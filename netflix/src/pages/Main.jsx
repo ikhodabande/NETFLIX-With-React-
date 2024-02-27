5e357b929ebb299194c0811ab2fc5ee919c2f260
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-27 12:48:19 +0330

    added style to buttons

--- netflix/src/pages/Main.jsx
+++ netflix/src/pages/Main.jsx
@@ -22,7 +22,13 @@ function Main() {
     <div className='w-full h-[550px] text-white'>
       <div className='w-full h-full'>
         <div className='absolute w-full h-full bg-gradient-to-r from-black'></div>
-        <img className='w-full h-[550px]  object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+        <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+        <div className='absolute w-full top-[20%]'>
+          <div>
+            <button className='text-black border border-gray-300 bg-white px-5 py-2 ml-4'>Play</button>
+            <button className='text-white border border-gray-300 py-2  px-5 ml-4'>Watch Later</button>
+          </div>
+        </div>
 
       </div>
 
