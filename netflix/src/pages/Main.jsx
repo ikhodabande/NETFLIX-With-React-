6ac3fa362ea06c7489ca6503474a0f690a249e31
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-27 14:56:03 +0330

    release date added as p tag

--- netflix/src/pages/Main.jsx
+++ netflix/src/pages/Main.jsx
@@ -23,15 +23,23 @@ function Main() {
       <div className='w-full h-full'>
         <div className='absolute w-full h-full bg-gradient-to-r from-black'></div>
         <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-        <div className='absolute w-full top-[20%]'>
-          <div>
-            <button className='text-black border border-gray-300 bg-white px-5 py-2 ml-4'>Play</button>
-            <button className='text-white border border-gray-300 py-2  px-5 ml-4'>Watch Later</button>
+        <div className='absolute w-full top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold '>
+            {movie?.title}
+            </h1>
+          <div className='my-4'>
+            <button 
+            className='text-black border border-gray-300 bg-white px-5 py-2 '>
+              Play
+              </button>
+            <button
+             className='text-white border border-gray-300 py-2  px-5 ml-4 '>
+              Watch Later
+              </button>
           </div>
+          <p className='text-gray-300 text-sm'>Released: {movie?.release_date}</p>
         </div>
-
       </div>
-
     </div>
   )
 }
