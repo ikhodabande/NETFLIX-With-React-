047294e35a57558b5e468ad65e76e86589d0c736
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-27 12:33:11 +0330

    add img and styled it

--- netflix/src/pages/Main.jsx
+++ netflix/src/pages/Main.jsx
@@ -5,19 +5,26 @@ import axios from 'axios';
 function Main() {
   const [movies,setMovies] = useState([])
 
+  const movie = movies[Math.floor(Math.random() * movies.length)]
+
 
 
   useEffect(()=>{
     axios.get(requests.requestPopular).then((Response)=>{
-      setMovies(Response.data)
+      setMovies(Response.data.results)
 
     })
   },[])
-  console.log(movies)
+  console.log(movie)
 
 
   return (
-    <div>
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <div className='absolute w-full h-full bg-gradient-to-r from-black'></div>
+        <img className='w-full h-[550px]  object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+
+      </div>
 
     </div>
   )
