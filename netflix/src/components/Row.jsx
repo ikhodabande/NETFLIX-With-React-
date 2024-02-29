b51dc404a140ad13637085b42b20bcc988270e2b
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-29 19:18:50 +0330

    add row.jsx

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -11,17 +11,18 @@ function Row = (title, fetchURL) => {
     ).then((response) => {
     setMovies(response.data.results);
   });
- },[]);
+ },[fetchURL]);
+
+ console.log(movies)
 
 
   return (
     <>
     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
     <div className='relative flex items-center'>
-    <div id='slider'>
-    </div>
+       <div id={'slider'}>
 
-
+       </div>
     </div>
 
     </>
