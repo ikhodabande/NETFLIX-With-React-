4b55d332874651429417f28adb52eb7e2f6ac9a3
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-29 18:20:07 +0330

    add rows to home.jsx

--- netflix/src/pages/Home.jsx
+++ netflix/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Main from './Main';
 import Row from '../components/Row';
+import requests from '../requests';
 
 
 function Home() {
@@ -8,7 +9,11 @@ function Home() {
   return (
     <>
     <Main/>
-    <Row/>                
+    <Row title='Up Comming' fetchURL={requests.requestUpcoming}/>                
+    <Row title='Trending' fetchURL={requests.requestTrending}/>                
+    <Row title='Popular' fetchURL={requests.requestPopular}/>                
+    <Row title='Favorite' fetchURL={requests.requestFavorite}/>                
+    <Row title='Horror' fetchURL={requests.requestHorror}/>                
     </>
   )
 }
