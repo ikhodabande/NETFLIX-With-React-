15ba33eedc55b2ca5c9c3fb2d77f007c3a58b004
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-29 18:11:19 +0330

    add useeffect to row.jsx

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -1,16 +1,24 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import requests from '../requests';
 
 function Row() {
  const [movies,setMovies] = useState([])
 
 
  useEffect(()=>{
-
- })
+  axios.get(requests.requestPopular).then((response) => {
+    setMovies(response.data.results);
+  });
+ },[]);
 
 
   return (
     <>
+    <div className='w-full h-[550] text-white'>
+      
+
+    </div>
 
     </>
   )
