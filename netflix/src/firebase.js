d5784db6143cf3c8fb2ae1cdbfdbecdb86243309
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-03 22:30:10 +0330

    install firebase and add firebase.js

---
+++ netflix/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqPJ25V6r0UDDu1lca5AwelE6MwlHemwQ",
+  authDomain: "netflix-react-kh.firebaseapp.com",
+  projectId: "netflix-react-kh",
+  storageBucket: "netflix-react-kh.appspot.com",
+  messagingSenderId: "75741703364",
+  appId: "1:75741703364:web:5496a12b882381a3536d91"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
