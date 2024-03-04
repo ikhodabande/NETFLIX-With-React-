ef5fea446839ae07dec032476b5115a6c744e409
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-04 04:57:09 +0330

    adding firebase and env file

--- netflix/src/firebase.js
+++ netflix/src/firebase.js
@@ -5,13 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqPJ25V6r0UDDu1lca5AwelE6MwlHemwQ",
-  authDomain: "netflix-react-kh.firebaseapp.com",
-  projectId: "netflix-react-kh",
-  storageBucket: "netflix-react-kh.appspot.com",
-  messagingSenderId: "75741703364",
-  appId: "1:75741703364:web:5496a12b882381a3536d91"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
 };
+
+// REACT_APP_FIREBASE_API_KEY=AIzaSyBqPJ25V6r0UDDu1lca5AwelE6MwlHemwQ
+// REACT_APP_FIREBASE_AUTH_DOMAIN=netflix-react-kh.firebaseapp.com
+// REACT_APP_FIREBASE_PROJECT_ID=netflix-react-kh
+// REACT_APP_FIREBASE_STORAGE_BUCKET=netflix-react-kh.appspot.com
+// REACT_APP_FIREBASE_MESSAGING_SENDER=75741703364
+// REACT_APP_APP_ID=1:75741703364:web:5496a12b882381a3536d91
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
