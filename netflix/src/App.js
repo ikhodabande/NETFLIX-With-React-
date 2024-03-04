bfa51eb7fef73da6dc1d048d29da9e5b654182cc
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-04 18:33:10 +0330

    add datas to authc ontext

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import {Routes,Route}  from 'react-router-dom'
+import { AuthContextprovider } from './context/AuthContext';
 
 function App() {
   return (
 
     <>
+    <AuthContextprovider>
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>} />
     </Routes>
+    </AuthContextprovider>
     </>
     
   );
--- netflix/src/context/AuthContext.js
+++ netflix/src/context/AuthContext.js
@@ -1,11 +1,44 @@
 import {creatContext, useContext, useEffect, useState} from 'react';
+import { Auth } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+   signInWithEmailAndPassword, 
+   signOut,
+   onAuthStateChanged
+} from 'firebase/auth'
+import { auth, auth, auth } from '../firebase';
 
 const AuthContext = creatContext()
 
 export function AuthContextprovider({children}){
+  const [user, setUser] = useState({})
 
+  function signUp(email,password){
+    return createUserWithEmailAndPassword(auth,email,password);
+  }
+
+  function logIn(email,password){
+    return signInWithEmailAndPassword(auth,email,password)
+
+  }
+  
+  function logOut(){
+    return signOut(auth)
+  }
+
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+      setUser(currentUser)
+    });
+    return ()=>{
+      unsubscribe();
+    };
+  });
+
+  
   return (
-   <AuthContext.Provider>
+   <AuthContext.Provider value={{signUp, logIn, logOut, user}}>
        {children}
    </AuthContext.Provider>
 )
