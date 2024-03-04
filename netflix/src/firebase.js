6a59ce5de54e60da6f05d27ad89ad099dbc3e51e
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-04 05:05:05 +0330

    import getAuth

--- netflix/src/firebase.js
+++ netflix/src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +22,6 @@ const firebaseConfig = {
 // REACT_APP_APP_ID=1:75741703364:web:5496a12b882381a3536d91
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
